0800a8e2781b8c2d3f77919cc1978590e70d4648
DDongYul <comeme0101@naver.com>
2023-02-09 17:48:31 +0900

    백트래킹 코드개선

--- backtracking/백트래킹-백준15686.py
+++ backtracking/백트래킹-백준15686.py
@@ -3,36 +3,28 @@ input = sys.stdin.readline
 from itertools import combinations
 INF = 1e9
 
-def dist(x,y):
-    for i in range(N):
-        for j in range(N):
-            if graph[i][j] == 1:
-                rst[i][j] = min(rst[i][j],abs(x-i)+abs(y-j))
-
 N,M = map(int,input().split())
+
 graph = []
 for _ in range(N):
     graph.append(list(map(int,input().split())))
-
 chicken = []
+
+house = []
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 2:
             chicken.append((i,j))
-            graph[i][j] = 0
-
+        elif graph[i][j] == 1:
+            house.append((i,j))
 comb = list(combinations(chicken,M))
 
 result = INF
 for c in comb:
-    rst = [[INF for _ in range(N)] for _ in range(N)]
+    temp = [INF for _ in range(len(house))]
     for dx,dy in c:
-         dist(dx,dy)
-    temp = 0
-    for i in range(N):
-        for j in range(N):
-            if rst[i][j] != INF:
-                temp+=rst[i][j]
-    result = min(result,temp)
+        for index,h in enumerate(house):
+            temp[index] = min(temp[index],(abs(dx-h[0])+abs(dy-h[1])))
+    result = min(result,sum(temp))
 
 print(result)
