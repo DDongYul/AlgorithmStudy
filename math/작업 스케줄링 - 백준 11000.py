a4179b15eb18f0ef2f23316b2c6f03f9eaad8651
DDongYul <comeme0101@naver.com>
2023-09-11 16:15:09 +0900

    작업 스케줄링-코드 리팩토링

--- math/작업 스케줄링 - 백준 11000.py
+++ math/작업 스케줄링 - 백준 11000.py
@@ -1,21 +1,23 @@
 import sys
+
 input = sys.stdin.readline
 from heapq import heappush, heappop
 
 N = int(input())
-work = []
+works = []
 for _ in range(N):
-    a,b = map(int,input().split())
-    heappush(work, (a,b))
+    a, b = map(int, input().split())
+    works.append((a, b))
+
+works.sort()
 
 # 각 강의실은 끝나는 시간만 기억해두면 됨
 room = [0]
-while work:
-    s,e = heappop(work) # 작업 스케쥴링: 빠른 시작시간 작업을 우선(Earliest start time first) 배정
+for s, e in works:
     curr = heappop(room)
-    if curr>s:
-        heappush(room,curr)
-        heappush(room,e)
+    if curr > s:
+        heappush(room, curr)
+        heappush(room, e)
     else:
-        heappush(room,e)
+        heappush(room, e)
 print(len(room))
